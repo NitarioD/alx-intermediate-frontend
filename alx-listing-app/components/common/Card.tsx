d2b5f77f7b57b4ad3530f76d04edc5d0048dd056
Jola <nitariod@gmail.com>
2025-11-09 00:36:49 +0100

    complete project alx listing app

---
+++ alx-listing-app/components/common/Card.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card = () => {
+  return <div>My Card Component</div>;
+};
+
+export default Card;
---
+++ alx-listing-app/constants/index.ts
@@ -0,0 +1,5 @@
+export const CONFIGURTION_SETTINGS = {
+  apiEndpoint: "https://api.example.com",
+  timeout: 5000,
+  maxRetries: 3,
+};
